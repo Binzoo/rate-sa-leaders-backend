07ad5441285e47472de85ef97a45b341d2044820
Binzoo <binamrathapa015@gmail.com>
2025-05-21 02:07:47 +0200

    Added Proper Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "https://rate-sa-leaders-backend.onrender.com"
     resource "*",
       headers: :any,
       expose: ["Authorization"],
